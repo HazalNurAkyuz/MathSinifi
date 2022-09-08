3854d583da9790955d8e99f9ba6851912f668d1b
Hazal Nur Akyuz <hazalnurakyuz@outlook.com>
2022-09-08 12:11:22 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Math sınıfı metotları
+        // Math.ceil(x) => bir büyük tam sayıya yuvarlama yapar
+        // Math.cos(x) => cos değerini döner
+        // Math.sin(x) => sin değerini döner
+        // Math.floor(x) => küçük tam sayıya yuvarlar
+        // Math.max(x,y) => büyük sayıyı verir
+        // Math.min(x,y) => küçük sayıyı verir
+        // Math.pow(3,5) => 3^5 üs alır
+        // Math.random() => 1 ve 0 arasında rastgele değer
+        // Math.round(x) => yakın olan tam sayıya döner
+        // Math.abs(x) => mutlak değer
+        // Math.sqrt(x) => karekök alma
+
+
+        System.out.println(Math.ceil(1.1));
+        System.out.println(Math.cos(30));
+        System.out.println(Math.floor(1.1));
+        System.out.println(Math.max(1.2, 1));
+        System.out.println(Math.pow(2, 4));
+        System.out.println(Math.round(1.6));
+        System.out.println(Math.abs(-1));
+        System.out.println(Math.sqrt(16));
     }
 }
